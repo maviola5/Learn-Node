e0dd6f47cdb2c12c5ca409301b9bd1e335213ba5
drogo <tviola@nacdonline.org>
2018-08-07 09:33:58 -0400

    left off with forms

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,3 +1,6 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.myMiddleware = (req, res, next) => {
     req.name = 'Wes';
     next();
@@ -5,4 +8,13 @@ exports.myMiddleware = (req, res, next) => {
 exports.homePage = (req, res) => {
     console.log(req.name)
     res.render('index');
+}
+
+exports.addStore = (req, res) => {
+    res.render('editStore', {title: 'Add Store'});
+}
+
+exports.createStore = (req, res) => {
+    const store = new Store(req.body);
+    store.save();
 }
--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+
+router.post('/add', storeController.createStore)
 
 router.get('/reverse/:name', (req, res) => {
   const reverse = [...req.params.name].reverse().join('');
